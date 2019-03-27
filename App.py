4fd0a3733ecc8c1d03a578193d6f07792be8126d
Radiyya Dwisaputra <radiyyasaputra@gmail.com>
2019-03-27 14:13:31 +0700

    Add main program + edit elgamal

--- App.py
+++ App.py
@@ -1,16 +1,37 @@
 from ElGamal import *
 from ElGamalKey import *
+from ECC import *
 
-p = int(input('Masukkan nilai p : '))
-g = int(input('Masukkan nilai g : '))
-x = int(input('Masukkan nilai x : '))
+print('=====ElGamal + ECCEG=====')
+print('1. Enkripsi ElGamal')
+print('2. Dekripsi ElGamal')
+print('3. Enkripsi ECCEG')
+print('4. Dekripsi ECCEG')
 
-ek = ElGamalKey(p,g,x)
-ek.saveKey('a','b')
-e = ElGamal('small_cat.jpg','a.pub')
-e.encryption('encrypted',1520)
-e.decryption('encrypted', 'b.priv')
-# block = e.split_into_block()
+opt = int(input('Masukkan pilihan enkripsi/dekripsi : '))
+if opt == 1:
+    p = int(input('Masukkan nilai p : '))
+    g = int(input('Masukkan nilai g : '))
+    x = int(input('Masukkan nilai x : '))
+    k = int(input('Masukkan nilai k : '))
+    ek = ElGamalKey(p,g,x)
+    stringfile = str(input('Masukkan nama File yang di enkrip : '))
+    cipherfile = str(input('Masukkan nama File hasil enkrip : '))
+    publicfilename = str(input('Masukkan nama file public key (tanpa extension) : '))
+    privatefilename = str(input('Masukkan nama file private key (tanpa extension) : '))
+    ek.saveKey(publicfilename,privatefilename)
+    e = ElGamal()
+    e.encryption(stringfile,cipherfile,publicfilename + '.pub',k)
 
+if opt == 2:
+    e = ElGamal()
+    privatefilename = str(input('Masukkan nama file private key (tanpa extension) : '))
+    encryptedfilename = str(input('Masukkan nama file yang akan di decrypt : '))
+    decryptedfilename = str(input('Masukkan nama file hasil decrypt : '))
+    e.decryption(encryptedfilename, decryptedfilename, privatefilename + '.priv')
 
+if opt == 3:
+    print('menu 3')
 
+if opt == 4:
+    print('menu 4')
--- ElGamal.py
+++ ElGamal.py
@@ -1,55 +1,53 @@
 from Tools import *
 
 class ElGamal:
-    def __init__(self, plainfilename, pubfilename):
-        string = ''
+    def __init__(self):
+        pass
+
+    def encryption(self, plainfilename, filename, pubfilename, k):
         len_bytes = []
+        plainString = ''
         for i in readFromFileBytes(plainfilename):
             len_bytes.append(len(str(i)))
         for c in readFromFileBytes(plainfilename):
-            string += str(int(c))
-
-        self.publickey = readFromFileString(pubfilename).split("\n")
-        self.plainString = string
-        self.len_bytes = len_bytes
-
-    def __split_into_block(self):
-        block = split_into_block(self.plainString, int(self.publickey[2]))
-        return  block
-
-    def encryption(self, filename, k):
+            plainString += str(int(c))
         encrypted = []
-        block = self.__split_into_block()
+        publickey = readFromFileString(pubfilename).split("\n")
+        block = split_into_block(plainString, int(publickey[2]))
         for i in block:
-            a = (int(self.publickey[1]) ** k) % int(self.publickey[2])
-            b = ((int(self.publickey[0]) ** k) * int(i)) % int(self.publickey[2])
+            a = (int(publickey[1]) ** k) % int(publickey[2])
+            b = ((int(publickey[0]) ** k) * int(i)) % int(publickey[2])
             encrypted.append(a)
             encrypted.append(b)
-        saveArrayToFile(filename,encrypted)
+        hex_encrypted=[]
+        for i in encrypted:
+            hex_encrypted.append(hex(i)[2:])
+        print('Hasil Enkripsi : ')
+        for i in hex_encrypted:
+            print(i,end='')
+        hex_encrypted.append(len_bytes)
+        saveArrayToFile(filename,hex_encrypted)
     
-    def decryption(self, encrypted_filename, privatefilename):
-        encrypted = readFromFileString(encrypted_filename).split('\n')
-        encrypted = split_string_into_list_of_length_n(encrypted[:-1],2)
+    def decryption(self, encrypted_filename, decryted_filename, privatefilename):
+        temp = readFromFileString(encrypted_filename).split('\n')[:-2]
+        encrypted = []
+        for i in temp:
+            encrypted.append(int(i,16))
+        len_bytes = readFromFileString(encrypted_filename).split('\n')[-2].replace('[','').replace(']','').split(',')
+        encrypted = split_string_into_list_of_length_n(encrypted,2)
         result = ''
         privatekey = readFromFileString(privatefilename).split('\n')
         for i in encrypted:
-            temp = (int(i[0])**(int(self.publickey[2])-int(privatekey[0])-1)) % int(privatekey[1])
+            temp = (int(i[0])**(int(privatekey[1])-int(privatekey[0])-1)) % int(privatekey[1])
             result += str((int(i[1])*temp)%int(privatekey[1]))
         i=0
         j=0
         resultint = []
         while i < len(result):
-            resultint.append(int(result[i:i+self.len_bytes[j]]))
-            i+=self.len_bytes[j]
+            resultint.append(int(result[i:i+int(len_bytes[j])]))
+            i+=int(len_bytes[j])
             j+=1
         resultbin = bytes(resultint)
-        saveBinToFile('decrypted',resultbin)
-
-        
-
-
-
-
-            
-        
-    
+        print('hasil dekripsi:')
+        print(resultbin)
+        saveBinToFile(decryted_filename,resultbin)
